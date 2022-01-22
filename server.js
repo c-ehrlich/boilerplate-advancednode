3b530e59e772c057a7774369179ad19236e31250
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-01-22 13:21:00 +0100

    implement initial user serializer and deserializer

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const myDB = require("./connection");
 const session = require("express-session");
 const passport = require("passport");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const Db = require("mongodb/lib/db");
 
 const app = express();
 
@@ -13,14 +14,27 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "pug");
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user._id); // just return user._id
+});
+
+passport.deserializeUser((id, done) => {
+  // db.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+  //   done(null, null);
+  // });
+  done(null, null);
+});
 
 app.route("/").get((req, res) => {
   // can also do process.pwd() + "/views/pug/index"
