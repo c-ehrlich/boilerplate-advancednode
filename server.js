8a6750c505bf600308c48fb83a6edd0aaf45f917
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-01-26 16:01:47 +0100

    implement user registration

--- server.js
+++ server.js
@@ -70,6 +70,7 @@ myDB(async (client) => {
       title: "Connected to Database",
       message: "Please login",
       showLogin: true,
+      showRegistration: true,
     });
   });
 
@@ -82,22 +83,48 @@ myDB(async (client) => {
       }
     );
 
+  app.route("/logout").get((req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
+
   app.route("/profile").get(ensureAuthenticated, (req, res) => {
     res.render("pug/profile", {
       username: req.user.username,
     });
   });
 
-  app.route("/logout").get((req, res) => {
-    req.logout();
-    res.redirect("/");
-  });
+  app.route("/register").post(
+    (req, res, next) => {
+      myDataBase.findOne({ username: req.body.username }, (err, user) => {
+        if (err) {
+          next(err);
+        } else if (user) {
+          res.redirect("/");
+        } else {
+          myDataBase.insertOne(
+            { username: req.body.username, password: req.body.password },
+            (err, doc) => {
+              if (err) {
+                res.redirect("/");
+              } else {
+                // the inserted document is held within the ops property of the doc
+                next(null, doc.ops[0]);
+              }
+            }
+          );
+        }
+      });
+    },
+    passport.authenticate("local", { faliureRedirect: "/" }),
+    (req, res, next) => {
+      res.redirect("/profile");
+    }
+  );
 
   app.use((req, res, next) => {
-    res.status(404)
-    .type("text")
-    .send("Not Found");
-  })
+    res.status(404).type("text").send("Not Found");
+  });
 }).catch((e) => {
   app.route("/").get((req, res) => {
     res.render("pug", {
