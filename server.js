3242bde552b7a7ce971c720a68ead9ebd738ede3
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-01-26 16:14:03 +0100

    implement password hashing

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const Db = require("mongodb/lib/db");
 const ObjectID = require("mongodb").ObjectID;
 const LocalStrategy = require("passport-local");
+const bcrypt = require("bcrypt");
 
 const app = express();
 
@@ -58,7 +59,7 @@ myDB(async (client) => {
         console.log("User " + username + " attempted to log in.");
         if (err) return done(err);
         if (!user) return done(null, false);
-        if (password !== user.password) return done(null, false);
+        if (!bcrypt.compareSync(password, user.password)) return done(null, false);
         return done(null, user);
       });
     })
@@ -102,8 +103,9 @@ myDB(async (client) => {
         } else if (user) {
           res.redirect("/");
         } else {
+          const hash = bcrypt.hashSync(req.body.password, 12);
           myDataBase.insertOne(
-            { username: req.body.username, password: req.body.password },
+            { username: req.body.username, password: hash },
             (err, doc) => {
               if (err) {
                 res.redirect("/");
