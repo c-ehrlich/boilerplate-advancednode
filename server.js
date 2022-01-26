1139a1c00476c19d57e0b1992d34997da04242c8
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-01-26 19:15:36 +0100

    authentication with socket.io

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const session = require("express-session");
 const passport = require("passport");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const Db = require("mongodb/lib/db");
+const passportSocketIo = require("passport.socketio");
+const MongoStore = require("connect-mongo");
+const cookieParser = require("cookie-parser");
 
 const routes = require("./routes.js");
 const auth = require("./auth.js");
@@ -15,6 +18,11 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
+// initialize memory store for websocket auth
+const MongoStore = require("connect-mongo")(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
+
 fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
@@ -23,13 +31,38 @@ app.set("view engine", "pug");
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
+    key: "express.sid", // cookie name
     resave: true,
+    store: store,
     saveUninitialized: true,
     cookie: { secure: false },
   })
 );
+
 app.use(passport.initialize());
 app.use(passport.session());
+
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: "express.sid", // cookie name
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail,
+  })
+);
+
+const onAuthorizeSuccess = (data, accept) => {
+  console.log("successful connection to socket.io");
+  accept(null, true);
+}
+
+const onAuthorizeFail = (data, message, error, accept) => {
+  if (error) throw new Error(message);
+  console.log("Failed connection to socket.io:", message);
+  accept(null, false);
+}
 
 myDB(async (client) => {
   const myDataBase = await client.db("database").collection("users");
@@ -38,19 +71,18 @@ myDB(async (client) => {
   auth(app, myDataBase);
 
   let currentUsers = 0;
-
   // takes 1. string containing the title of the emitted event 2. function with which the data is passed through
   // a socket is an individual client who is connected
   io.on("connection", (socket) => {
     ++currentUsers;
-    io.emit('user count', currentUsers);
-    console.log("A user has connected");
+    io.emit("user count", currentUsers);
+    console.log("user " + socket.request.user.name + " connected");
 
     socket.on("disconnect", () => {
       console.log("A user has disconnected");
       --currentUsers;
       io.emit("user count", currentUsers);
-    })
+    });
   });
 }).catch((e) => {
   app.route("/").get((req, res) => {
